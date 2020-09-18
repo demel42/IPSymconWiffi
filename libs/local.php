30f72b7da745658aa8799109ce12f579bb2a8702
IP-Symcon <root@ips-prod>
2020-09-18 11:36:51 +0200

    libs/local.php war nicht added

---
+++ libs/local.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+trait WiffiLocalLib
+{
+    public static $IS_INVALIDCONFIG = IS_EBASE + 1;
+    public static $IS_MODULETYPEMISMATCH = IS_EBASE + 2;
+
+    public static $WIFFI_MODULE_NONE = 0;
+    public static $WIFFI_MODULE_WZ = 1;
+    public static $WIFFI_MODULE_3 = 2;
+
+    private function GetFormStatus()
+    {
+        $formStatus = [];
+        $formStatus[] = ['code' => IS_CREATING, 'icon' => 'inactive', 'caption' => 'Instance getting created'];
+        $formStatus[] = ['code' => IS_ACTIVE, 'icon' => 'active', 'caption' => 'Instance is active'];
+        $formStatus[] = ['code' => IS_DELETING, 'icon' => 'inactive', 'caption' => 'Instance is deleted'];
+        $formStatus[] = ['code' => IS_INACTIVE, 'icon' => 'inactive', 'caption' => 'Instance is inactive'];
+        $formStatus[] = ['code' => IS_NOTCREATED, 'icon' => 'inactive', 'caption' => 'Instance is not created'];
+
+        $formStatus[] = ['code' => self::$IS_INVALIDCONFIG, 'icon' => 'error', 'caption' => 'Instance is inactive (invalid configuration)'];
+        $formStatus[] = ['code' => self::$IS_MODULETYPEMISMATCH, 'icon' => 'error', 'caption' => 'Instance is inactive (wrong wiffi-module)'];
+
+        return $formStatus;
+    }
+}
